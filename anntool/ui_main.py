bac65dd57c63880b8a5884a660142b8880c66680
ayushmankumar7 <ayushmankumar7@gmail.com>
2021-09-20 23:47:26 +0530

    close minimize maximize functions added :

--- anntool/ui_main.py
+++ anntool/ui_main.py
@@ -1,40 +1,38 @@
 from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QGraphicsDropShadowEffect, QApplication
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(350, 200, 431, 161))
-        self.label.setObjectName("label")
-        self.mdiArea = QtWidgets.QMdiArea(self.centralwidget)
-        self.mdiArea.setGeometry(QtCore.QRect(40, 130, 200, 160))
-        self.mdiArea.setObjectName("mdiArea")
-        self.dial = QtWidgets.QDial(self.centralwidget)
-        self.dial.setGeometry(QtCore.QRect(70, 370, 50, 64))
-        self.dial.setObjectName("dial")
-        self.dateTimeEdit = QtWidgets.QDateTimeEdit(self.centralwidget)
-        self.dateTimeEdit.setGeometry(QtCore.QRect(200, 390, 194, 22))
-        self.dateTimeEdit.setObjectName("dateTimeEdit")
-        self.horizontalSlider = QtWidgets.QSlider(self.centralwidget)
-        self.horizontalSlider.setGeometry(QtCore.QRect(330, 340, 160, 22))
-        self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
-        self.horizontalSlider.setObjectName("horizontalSlider")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+from src.mainwindow import Ui_MainWindow
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.minimize_button.clicked.connect(self.minimize)
+        self.ui.maximize_restore_button.clicked.connect(self.maximize)
+        self.ui.close_button.clicked.connect(self.close_window)
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
 
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "Brother ! Here I am"))
+        self.state = 0
+
+    def close_window(self):
+        self.close()
+
+    def minimize(self):
+        self.showMinimized()
+    
+    def maximize(self):
+        if self.state == 0:
+            self.showMaximized()
+            self.state = 1 
+        else:
+            self.showNormal()
+            self.state = 0
+
+if __name__ == "__main__":
+    import sys 
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec_())

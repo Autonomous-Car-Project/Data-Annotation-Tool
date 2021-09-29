644a584e4686e166d303dd354bd8771f1b0414b4
HarshitaRaj17 <88398133+HarshitaRaj17@users.noreply.github.com>
2021-09-29 20:22:32 +0530

    functions added in main.py

--- anntool/main.py
+++ anntool/main.py
@@ -1,23 +1,84 @@
 import sys
-from PyQt5 import QtCore
+from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import QMainWindow, QGraphicsDropShadowEffect, QApplication
-
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtCore import QDate, QTime, QDateTime, Qt
+from datetime import date
+from datetime import datetime
+from PyQt5.QtCore import QTimer, QTime, Qt
+import glob
 from ui_splash import Ui_SplashScreen
-
 from src.mainwindow import Ui_MainWindow
-
-counter = 0
-
+counter=0
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        QtCore.QTimer.singleShot(1500, lambda: self.ui.date_label.setText("<strong>Successful</strong>"))
-        QtCore.QTimer.singleShot(1500, lambda: self.setStyleSheet("background-color: #222; color: #FFF"))
+        #QtCore.QTimer.singleShot(1500, lambda: self.ui.date_label.setText("<strong>Successful</strong>"))
+        #QtCore.QTimer.singleShot(1500, lambda: self.setStyleSheet("background-color: #222; color: #FFF"))
+        self.ui.minimize_button.clicked.connect(self.minimize)
+        self.ui.maximize_restore_button.clicked.connect(self.maximize)
+        self.ui.close_button.clicked.connect(self.close_window)
+        self.ui.imgPath_dropDown_button.clicked.connect(self.dialog_box)
 
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        timer = QTimer(self)
+        timer.timeout.connect(self.showTime)
+        timer.start(1000) # update every second
+        self.state = 0
+        self.date()
+        self.showTime()
+
+
+       
+    def close_window(self):
+        self.close()
+
+    def minimize(self):
+        self.showMinimized()
+    
+    def maximize(self):
+        if self.state == 0:
+            self.showMaximized()
+            self.state = 1 
+        else:
+            self.showNormal()
+            self.state = 0
+    def date(self):
+        
+        now = datetime.now()
+        self.dat=now.strftime("%d %B,%Y")
+      
+        if(self.dat[1]=='1' and self.dat[0]!='1'):
+            self.dat=now.strftime("%dst %B,%Y")
+        elif(self.dat[1]=='2' and self.dat[0]!='1'):
+            self.dat=now.strftime("%dnd %B,%Y")
+        elif(self.dat[1]=='3' and self.dat[0]!='1'):
+            self.dat=now.strftime("%drd %B,%Y") 
+        else:
+            self.dat=now.strftime("%dth %B,%Y")                   
+        _translate = QtCore.QCoreApplication.translate
+        self.ui.date_label.setText(_translate(" ", self.dat))   
+
+
+    def showTime(self):
+        self.currentTime = QTime.currentTime()
+
+        self.displayTxt = self.currentTime.toString('hh:mm')
+        
+
+        self.ui.time_label.setText(self.displayTxt)
+        
+    def dialog_box(self):
+            
+        openfile =str(QFileDialog.getExistingDirectory(self, "Select a Folder"))
+        print(openfile)
+        a=glob.glob(openfile+"/*.*")
+        print(a)
 
 class SplashScreen(QMainWindow):
     def __init__(self):
